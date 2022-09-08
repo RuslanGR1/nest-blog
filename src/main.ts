d242d33b1d1e5416880264adb157b84ea209dcdc
Ruslan G <almetfly@gmail.com>
2022-09-08 17:31:53 +0300

    add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 require('dotenv').config()
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Blog')
+    .setDescription('The blog API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3005);
 }
 bootstrap();
